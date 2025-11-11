4c6846c427cdfbf9290b6a68dd36b6b328f4c334
Daniel Choi <danielchoi1115@gmail.com>
2025-11-11 15:03:50 +0900

    Spring boot sample 업데이트

--- src/main/java/com/winten/greenlight/sample/greenlight/GreenlightService.java
+++ src/main/java/com/winten/greenlight/sample/greenlight/GreenlightService.java
@@ -3,11 +3,11 @@ package com.winten.greenlight.sample.greenlight;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URLEncoder;
@@ -15,38 +15,39 @@ import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
+@Slf4j
 public class GreenlightService {
-    private static final String TOKEN_COOKIE_NAME = "X-GREENLIGHT-TOKEN";
-    private static final String GREENLIGHT_BASE = "https://api.greenlight.hyundai-ite.com";
-//    private static final String GREENLIGHT_BASE = "http://localhost:18080";
+    private static final String TOKEN_COOKIE_NAME = "X-GREENLIGHT-TOKEN"; // greenlight token 쿠기명, 고정값
+    private static final String TOKEN_QUERY_KEY = "gUserId"; // greenlight 토큰 쿼리명, 고정값
+    private static final String GREENLIGHT_BASE = "https://api.greenlight.hyundai-ite.com"; // 그린라이트 API URL, 고정값
+
+    private static final Set<String> TARGET_URLS = Set.of("/itemPtc"); // 영향도 최소화를 위한 대기열 대상 URL 제한, POC를 위한 임시 세팅값
+    private static final String POC_LANDING_ID = ""; // 제공된 ID 입력, POC를 위한 임시 세팅값
 
     private final RestTemplate restTemplate = new RestTemplate();
 
     public boolean checkEntry(HttpServletRequest request, HttpServletResponse response) throws Exception {
         // 영향도 최소화를 위한 대기열 부분 적용처리
-        // 인터셉터에서 pattern으로 제외하는 방법이 불가능할 경우 수동으로 적용하는 방법도 가능
+        // 인터셉터에서 pattern으로 제외하는 방법도 있으나, 여기서는 간단하게 수동으로 적용
         if (!isGreenlightRequired(request)) {
             return true;
         }
 
         // 토큰 추출
-        String tokenFromCookie = null;
-        var paramMap = request.getParameterMap();
-        if (paramMap.get("g") != null && (paramMap.get("g").length > 0)) {
-            tokenFromCookie = paramMap.get("g")[0];
-        }
-        if (tokenFromCookie == null) {
-            tokenFromCookie = readCookie(request, TOKEN_COOKIE_NAME);
+        String greenlightToken = readQuery(request, TOKEN_QUERY_KEY);
+        if (greenlightToken == null) {
+            greenlightToken = readCookie(request, TOKEN_COOKIE_NAME);
         }
 
         // 토큰이 있다면 검증 시도
-        if (tokenFromCookie != null && !tokenFromCookie.isBlank()) {
+        if (greenlightToken != null && !greenlightToken.isBlank()) {
             // 대기열 대기요청 API 헤더 세팅
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_JSON);
             headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-            headers.add(TOKEN_COOKIE_NAME, tokenFromCookie); // X-GREENLIGHT-TOKEN 헤더
+            headers.add(TOKEN_COOKIE_NAME, greenlightToken); // X-GREENLIGHT-TOKEN 헤더
 
             // 현재 URL 전달
             Map<String, String> body = new HashMap<>();
@@ -54,32 +55,42 @@ public class GreenlightService {
 
             ResponseEntity<QueueCheckResponse> res = null;
             try {
-                // 대기열 대기요청 API 호출
+                // 대기열 검증 요청 API 호출
                 res = restTemplate.postForEntity(
                         GREENLIGHT_BASE + "/api/v1/customer/verify",
                         httpEntity,
                         QueueCheckResponse.class
                 );
-            } catch (Exception e) {}
+            } catch (Exception e) {
+                log.warn(e.getMessage());
+            }
 
             if (res != null && res.getBody() != null) {
                 QueueCheckResponse result = res.getBody();
                 if (result.getVerified()) {
-                    return true;
+                    return true; // 여기까지 왔다면 정상적인 토큰을 가지고 검증에 성공해서 진입 가능한 경우
                 }
             }
-            }
+        }
 
-        String landingId = "0NFXE62QE2XCA";
+        // 이곳에 도달했다면 대기해야는 경우
         String currentUrl = buildCurrentUrl(request);
         // destinationUrl이 있으면 우선 사용, 없으면 현재 URL로 복귀하도록 redirect 파라미터로 전달(구현 환경에 맞게 조정)
-        String redirectTo = "https://greenlight.hyundai-ite.com/l/" + landingId + "?redirectUrl=" + URLEncoder.encode(currentUrl, StandardCharsets.UTF_8); // WAITING인 상태라면 대기화면으로 이동
+        String redirectTo = "https://greenlight.hyundai-ite.com/l/" + POC_LANDING_ID + "?redirectUrl=" + URLEncoder.encode(currentUrl, StandardCharsets.UTF_8); // WAITING인 상태라면 대기화면으로 이동
         response.sendRedirect(redirectTo);
         return false; // 더 이상 체인 진행 안 함
     }
 
+    private String readQuery(HttpServletRequest request, String queryKey) {
+        try {
+            return request.getParameterMap().get(queryKey)[0];
+        } catch (Exception ignored) {
+            return null;
+        }
+    }
+
     private boolean isGreenlightRequired(HttpServletRequest request) {
-        return "/itemPtc".equals(request.getRequestURI());
+        return TARGET_URLS.contains(request.getRequestURI());
     }
 
     // 현재 URL 추출. 유틸성 함수
